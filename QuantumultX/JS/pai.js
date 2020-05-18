644f2fb7b705b0e283396dfe303cc93f56ed54a5
ju0632 <ju0632@qq.com>
2020-05-18 20:18:52 +0800

    圆周率测试

---
+++ QuantumultX/JS/pai.js
@@ -0,0 +1,9 @@
+var start = Date.now(); 
+
+var pai = 0, flag = false;
+for(var i=1; i<30000000; i+=2) {
+ pai += ((flag = !flag) ? 1 : -1) * 1 / i;
+}
+console.log(’圆周率π：‘+pai*4);
+console.log(’用时:‘ + (Date.now() - start)+’ms‘)
+$done
