8a385ed3ad419d25c55823a76cc1d4a100a8a946
ju0632 <ju0632@qq.com>
2020-05-18 20:39:42 +0800

    更正

--- QuantumultX/JS/pai.js
+++ QuantumultX/JS/pai.js
@@ -4,6 +4,6 @@ var pai = 0, flag = false;
 for(var i=1; i<30000000; i+=2) {
  pai += ((flag = !flag) ? 1 : -1) * 1 / i;
 }
-console.log('圆周率：'+pai*4);
-console.log('用时:' + (Date.now() - start)+'ms')
+console.log('圆周率π：'+pai*4);
+console.log('用时：'+ (Date.now() - start)+'ms')
 $done
